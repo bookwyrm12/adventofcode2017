00372e3305ef6b2804c2286b99bed9bd25d8dd99
April Jackson <april.jackson012@gmail.com>
2017-12-05 00:20:08 -0800

    Solve day 5 part 1

---
+++ day05/day5.py
@@ -0,0 +1,38 @@
+#####################################
+#  Advent of Code 2017              #
+#  Day 5: A Maze of Twisty          #
+#         Trampolines, All Alike    #
+#  April Jackson                    #
+#####################################
+
+# Get the data
+with open('day5.in') as f:
+    data = f.read()
+
+
+# Part 1
+def part1(instructions):
+    instructions = [int(x) for x in instructions.splitlines()]
+    steps = 0
+    pointer = 0
+
+    while True:
+        after_jump = pointer + instructions[pointer]
+        instructions[pointer] += 1
+        pointer = after_jump
+        steps += 1
+
+        if pointer < 0 or pointer >= len(instructions):
+            break
+
+    print('Part 1: Steps to reach the exit: {}'.format(steps))
+
+
+# Part 2
+def part2(instructions):
+    pass
+
+
+# Do the stuff
+part1(data)
+part2(data)
