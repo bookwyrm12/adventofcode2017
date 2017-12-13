da6a8c5d3535372ac48446d6d7dfd87a1d6ff451
April Jackson <april.jackson012@gmail.com>
2017-12-12 20:43:46 -0800

    Solve day 12 part 1

---
+++ day12/day12.py
@@ -0,0 +1,52 @@
+#############################
+#  Advent of Code 2017      #
+#  Day 12: Digital Plumber  #
+#  April Jackson            #
+#############################
+
+
+# Get the data
+with open('day12.in') as f:
+    data = f.read()
+
+
+# Helper method: Loop around circular list, from end to beginning
+def data_to_dict(data):
+    data_dict = {}
+    for line in data.splitlines():
+        line = line.split()
+        id, programs = int(line[0]), line[2:]
+        data_dict[id] = programs
+    return data_dict
+
+
+# Part 1
+def part1(village):
+    village = data_to_dict(village)
+    seen = []
+
+    num_programs = part1_recurse(village, 0, seen)
+    print('Part 1: Number of programs: {}'.format(num_programs))
+
+
+def part1_recurse(village, id, seen):
+    # print('ID: {}; seen: {}; num_programs: {}'.format(id, seen, num_programs))
+    if id in seen:
+        return 0
+
+    seen.append(id)
+
+    for prog in village[id]:
+        tmp = part1_recurse(village, int(prog), seen)
+
+    return len(seen)
+
+
+# Part 2
+def part2(village):
+    pass
+
+
+# Do the stuff
+part1(data)
+part2(data)
